22228350b7acbf040cf7625fddc04c4ef080260b
saurabh-uchil <uchilsaurabh@gmail.com>
2021-10-17 19:30:27 +1100

    Validation done

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const bcrypt = require('bcryptjs')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+//Regex for validation
+const regexForEmail = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/
+const regexForNumbers = /\d/
+const regexForSpecialCharacters = /[-|$|=|_|(|)|{|}|:|;|'|"|.|>|<|,|!|@|#|%|^|&|?|\/|\\|\||~|`|*]/
+
 //Ports to listen
 const PORT = process.env.PORT || 3000
 
@@ -71,17 +76,52 @@ app.get('/users/:id/schedules', (req, res) =>{
 
 //Create new users
 app.post('/users', (req, res) => {
-    const salt = bcrypt.genSaltSync(10)
-    const password = bcrypt.hashSync(req.body.password, salt)
-    req.body.password = password 
-    data.users.push(req.body)
-    res.send(req.body)
+    //Validating inputs for user values
+    if(req.body.firstname==null||req.body.firstname==""||regexForNumbers.test(req.body.firstname)||regexForSpecialCharacters.test(req.body.firstname)){
+        res.send("Enter a valid first name")
+    }
+    else if(req.body.lastname==null||req.body.lastname==""||regexForNumbers.test(req.body.lastname)||regexForSpecialCharacters.test(req.body.lastname)){
+        res.send("Enter a valid last name")
+    }
+    else if(req.body.email==null||req.body.email==""||!regexForEmail.test(req.body.email)){
+        res.send("Enter a valid email address")
+    }
+    else if(req.body.password==null||req.body.password==""){
+        res.send("Password cannot be empty")
+    }
+    else{
+        const salt = bcrypt.genSaltSync(10)
+        const password = bcrypt.hashSync(req.body.password, salt)
+        req.body.password = password 
+        data.users.push(req.body)
+        res.send(req.body)
+    }    
 })
 
 //Create a new schedule
 app.post('/schedules', (req, res) => {
-    data.schedules.push(req.body)
-    res.send(req.body)
+
+    //Validating inputs for schedule values
+    if(req.body.user_id==null|| req.body.user_id==""){
+        res.send("User Id cannot be empty")
+    }
+    else if(isNaN(req.body.user_id)){
+        res.send("Enter a valid user id")
+    }
+    else if(req.body.day==""||req.body.day==null||isNaN(req.body.day)){
+        res.send("Enter a valid day")
+    }
+    else if(req.body.start_at==""||req.body.start_at==null){
+        res.send("Enter a valid start time")
+    }
+    else if(req.body.end_at==null||req.body.end_at==""){
+        res.send("Enter a valid end time")
+    }
+    else{
+        data.schedules.push(req.body)
+        res.send(req.body)
+    }
+   
 })
 
 //Listen on the given port
